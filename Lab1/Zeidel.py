4058d7fd7d16f95a1859e749b082d43b5832227c
Egor Gordionok <egordionok@yandex-team.ru>
2022-03-03 14:04:27 +0300

    1

--- Lab1/Zeidel.py
+++ Lab1/Zeidel.py
@@ -2,10 +2,6 @@ import tkinter as tk
 import tkinter.font as tkFont
 import numpy as np
 
-'''
-
-'''
-
 
 def vector_norm(x):
     m = abs(x[0])
@@ -13,6 +9,40 @@ def vector_norm(x):
         if abs(i) > m:
             m = abs(i)
     return m
+
+
+def SimpleIterations(arr):
+    A = [[arr[i][j] for j in range(len(arr[i]) - 1)] for i in range(len(arr))]
+    n = len(A)
+    B = [arr[i][len(A[0])] for i in range(n)]
+
+    # a_11*x1 + a_12*x2 + ... + a_1n*xn = b_1
+    # .....    ......   ...   ......  ...
+    # a_n1*x1 + a_n2*x2 + ... + a_nn*xn = b_n
+    #
+    #       ||
+    #       \/
+    #
+    # x1(k+1) = alpha_11*x1(k) + alpha_12*x2 + ... + alpha_1n*xn = betta_1
+    # .....    ......   ...   ......  ...
+    # xn = alpha_n1*x1 + alpha_n2*x2 + ... + alpha_nn*xn = betta_n
+
+    alpha = np.array([[-A[i][j] / A[i][i] for j in range(n)] for i in range(n)])
+    for i in range(n):
+        alpha[i][i] = 0
+    betta = np.array([B[i] / A[i][i] for i in range(n)])
+
+    x = np.copy(betta)
+    eps = 1
+    n = 0
+
+    while eps >= 0.00001:
+        x_last = x
+        x = np.dot(alpha, x) + betta
+        eps = vector_norm(x - x_last)
+        n += 1
+
+    return x, n
 
 
 def Zeidel(arr):
@@ -46,9 +76,9 @@ def Zeidel(arr):
     for i in range(n):
         for j in range(n):
             if j < i:
-                B[i][j] = alpha[i][j]   # Нижняя треугольная матрица с для коэф x_i(k+1) с 0 диагональю
+                B[i][j] = alpha[i][j]  # Нижняя треугольная матрица с для коэф x_i(k+1) с 0 диагональю
             else:
-                C[i][j] = alpha[i][j]   # Верхняя треугольная матрица с для коэф x_i(k)
+                C[i][j] = alpha[i][j]  # Верхняя треугольная матрица с для коэф x_i(k)
 
     # alpha = B + C
 
@@ -76,25 +106,36 @@ def Calculate():
     while (len(arr[len(arr) - 1])) == 0:
         arr.pop()
 
+    a1, n1 = SimpleIterations(arr)
     a2, n2 = Zeidel(arr)
+
+    ans1 = ''
+    for i in a1:
+        ans1 += str(round(i, 4)) + ' '
 
     ans2 = ''
     for i in a2:
         ans2 += str(round(i, 4)) + ' '
 
-    entry_roots.delete(0, tk.END)
-    entry_roots.insert(0, ans2)
-    label_n['text'] = str(n2)
+    entry_roots1.delete(0, tk.END)
+    entry_roots1.insert(0, ans1)
+    label_n1['text'] = str(n1)
+
+    entry_roots2.delete(0, tk.END)
+    entry_roots2.insert(0, ans2)
+    label_n2['text'] = str(n2)
 
 
 def Clean():
     table_coefs.delete('1.0', tk.END)
-    entry_roots.delete(0, tk.END)
-    label_n['text'] = ''
+    entry_roots1.delete(0, tk.END)
+    entry_roots2.delete(0, tk.END)
+    label_n1['text'] = ''
+    label_n2['text'] = ''
 
 
 window = tk.Tk()
-window.title('Решение СЛАУ методом Зейделя')
+window.title('Решение СЛАУ методом простых итераций')
 
 font_arial = tkFont.Font(family="Arial", size=14)
 
@@ -102,15 +143,29 @@ tk.Label(text="A|B =", font=font_arial).grid(row=0, column=0, sticky='e', pady=1
 table_coefs = tk.Text(width=21, height=10, font=font_arial)
 table_coefs.grid(row=0, column=1, columnspan=3, sticky='w', padx=10)
 
+tk.Label(text="Метод простых итераций", font=font_arial).grid(row=1, column=0, columnspan=4, sticky='we', padx=10,
+                                                              pady=10)
+
+tk.Label(text="x\U00002081, x\U00002082, ... :", font=font_arial).grid(row=2, column=0, sticky='e', padx=10, pady=10)
+entry_roots1 = tk.Entry(font=font_arial)
+entry_roots1.grid(row=2, column=1, columnspan=3, sticky='w', padx=10)
+
+tk.Label(text="Итераций:", font=font_arial).grid(row=3, column=0, sticky='e', padx=10, pady=10)
+label_n1 = tk.Label(font=font_arial)
+label_n1.grid(row=3, column=1, columnspan=3, sticky='w')
+
+tk.Label(text="Метод Зейделя", font=font_arial).grid(row=4, column=0, columnspan=4, sticky='we', padx=10, pady=10)
+
 tk.Label(text="x\U00002081, x\U00002082, ... :", font=font_arial).grid(row=5, column=0, sticky='e', padx=10, pady=10)
-entry_roots = tk.Entry(font=font_arial)
-entry_roots.grid(row=5, column=1, columnspan=3, sticky='w', padx=10)
+entry_roots2 = tk.Entry(font=font_arial)
+entry_roots2.grid(row=5, column=1, columnspan=3, sticky='w', padx=10)
 
 tk.Label(text="Итераций:", font=font_arial).grid(row=6, column=0, sticky='e', padx=10, pady=10)
-label_n = tk.Label(font=font_arial)
-label_n.grid(row=6, column=1, columnspan=3, sticky='w')
+label_n2 = tk.Label(font=font_arial)
+label_n2.grid(row=6, column=1, columnspan=3, sticky='w')
 
-tk.Button(text="Вычислить", command=Calculate, font=font_arial).grid(row=7, column=0, columnspan=2, pady=10, padx=10, sticky='w')
+tk.Button(text="Вычислить", command=Calculate, font=font_arial).grid(row=7, column=0, columnspan=2, pady=10, padx=10,
+                                                                     sticky='w')
 tk.Button(text="Очистить", command=Clean, font=font_arial).grid(row=7, column=3, padx=10, sticky='e')
 
 tk.mainloop()
