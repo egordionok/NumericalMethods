f00d7df7b35cbaab61210cfce6b683fdba145ac9
Egor Gordionok <84126576+egordionok@users.noreply.github.com>
2022-03-16 21:32:37 +0300

    1

--- Lab1/QR.py
+++ Lab1/QR.py
@@ -3,6 +3,19 @@ import tkinter.font as tkFont
 import numpy as np
 import math
 
+a = [
+    [-4, -6, -3],
+    [-1, 5, -5],
+    [6, 2, 5]
+]
+
+# a = [
+#     [1, 3, 1],
+#     [1, 1, 4],
+#     [4, 3, 1]
+# ]
+
+
 def sign(x):
     if x > 0:
         return 1
@@ -10,13 +23,47 @@ def sign(x):
         return 0
     return -1
 
-def QR(arr):
-    n = len(arr)
+
+def norma(vector):
+    return math.sqrt(sum([i*i for i in vector]))
+
+
+def Householder(arr):
     A = np.array(arr)
-    v = np.array()
-    v.append(A[0][0] + sign(A[0][0])*math.sqrt(sum([A[j][0]**2 for j in range(0, n)])))
-    for i in range(1, n):
-        v.append(A[i][0])
+    n = len(A)
 
-    H = np.eye(n) - 2 * np.dot(np.dot(v, v.T),np.invert(np.dot(v.T, v)))
+    H = []
+    for i in range(n - 1):
+        v = [A[j][i] if j >= i else 0 for j in range(n)]
+        v[i] = A[i][i] + sign(A[i][i])*norma(A[i:, i])
 
+        v = np.array(v)
+        # print('v:', v, sep='\n')
+
+        vt = v.reshape(len(v), 1)
+        v = v.reshape(1, len(v))
+
+        h = np.eye(n) - 2 * np.dot(vt, v)/np.dot(v, vt)
+        H.append(h)
+        A = np.dot(h, A)
+        # print('h:', h, 'A:', A, sep='\n')
+
+    Q = H[0]
+    for i in range(1, len(H)):
+        Q = np.dot(Q, H[i])
+
+    return Q, A
+
+
+a = np.array(a)
+n = len(a)
+i = 0
+while math.sqrt(sum([a[i][j]*a[i][j] for i in range(n) for j in range(n) if i > j])) > 0.001 and i < 1000:
+    q, r = Householder(a)
+    # print('Q:', q, 'R:', r, sep='\n')
+    a = np.dot(r, q)
+    # print('A:', a)
+    print(i)
+    i += 1
+
+print(a)
